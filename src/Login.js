1b9299563b3b16398afc8400ad483098b82f0574
Deng <donggdeng@gmail.com>
2021-11-08 13:30:43 +0800

    change logo src

--- src/Login.js
+++ src/Login.js
@@ -40,7 +40,7 @@ function Login() {
             <Link to='/'>
                 <img 
                 className="login__logo" 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" 
                 alt=""
                 />
             </Link>
